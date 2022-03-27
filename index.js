e9e44e1e1220ce128a67ff764f9916af59e34c83
Elna JP <eriosdd@gmail.com>
2022-03-27 19:44:29 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solution in this file!
+
+
+let companyName = "Scuber"
+companyName
+{
+const companyName = "Scuber"
+}
+      
+ let mostProfitableNeighborhood = 'Chelsea'
+ 
+ 
+    let companyCeo = 'Susan Smith'
+
+   
